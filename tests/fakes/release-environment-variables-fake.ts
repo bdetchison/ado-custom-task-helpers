77244666862a8af2be258540c9b0743779962025
Etchison, Brandon <Brandon.Etchison@adesa.com>
2020-09-22 22:36:28 -0400

    Add another release time variable

--- tests/fakes/release-environment-variables-fake.ts
+++ tests/fakes/release-environment-variables-fake.ts
@@ -6,8 +6,9 @@ export class ReleaseEnvironmentVariables {
     readonly id = "1";
     readonly name = "Release-1";
     readonly link = "https://arbitrary.visualstudio.com/72bad61c-ab56-45e0-b2dc-e019cce9f4a2/_release?releaseId=1&_a=release-summary";
-    readonly startTime = "2020-09-18 11:22:34-04:00";
+    readonly startTime = "2020-09-22 20:33:48Z";
     readonly accessToken = "arbitraryToken";
     readonly projectName = "arbitraryProjectName";
     readonly adoServerUri = "https://arbitrary.vsrm.visualstudio.com/";
+    readonly pipelineStartTime ="2020-09-22 21:04:36-04:00";
 }
